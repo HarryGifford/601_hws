deab96e5c8bf23cdf1b1a012c72c777f1018655e
Harry Gifford <cwillis@mba1.hsd1.pa.comcast.net>
2014-12-15 23:03:55 -0500

    Simplified LR code for students.

--- LR/minimize.m
+++ LR/minimize.m
@@ -22,11 +22,9 @@ function theta = minimize(f, theta, X, y, opt)
     end
     % set defaults for options that are not provided.
     if ~isfield(opt, 'tol'), opt.tol = 1e-6; end
-    if ~isfield(opt, 'MaxIter'), opt.MaxIter = 1000; end
-    if ~isfield(opt, 'alpha'), opt.alpha = 2; end
-    if ~isfield(opt, 'gamma'), opt.gamma = 0.7; end
+    if ~isfield(opt, 'MaxIter'), opt.MaxIter = 6000; end
+    if ~isfield(opt, 'alpha'), opt.alpha = 3; end
     if ~isfield(opt, 'alpha_decay'), opt.alpha_decay = 0.998; end
-    if ~isfield(opt, 'batch_size'), opt.batch_size = 500; end
     
     m = size(X, 1);
     
@@ -38,32 +36,17 @@ function theta = minimize(f, theta, X, y, opt)
     
     prev_cost = inf;
     alpha = opt.alpha;
-    v = zeros(size(theta));
     fprintf('Running Gradient Descent...\n');
     for iter = 1:opt.MaxIter
-        idxs = randperm(m);
-        X = X(idxs, :);
-        y = y(idxs, :);
-        for start_idx = 1:opt.batch_size:m            
-            end_idx = min(m, start_idx + opt.batch_size-1);
-            [cost, grad] = f(theta, X(start_idx:end_idx,:), y(start_idx:end_idx,:));
-            if isnan(cost) || cost > 1e30
-                prev_cost = inf;
-                theta = 0.01*randn(size(theta));
-                alpha = 0.5*alpha;
-                continue;
-            end
-            v = opt.gamma*v + alpha*grad;
-            theta = theta - v;
-            alpha = alpha*opt.alpha_decay;
-        end
-        cost = f(theta, X, y);
+        [cost, grad] = f(theta, X, y);
+        theta = theta - alpha*grad;
         if abs(cost - prev_cost) < opt.tol, break; end
         if mod(iter, 10) == 0,
-            fprintf('Running epoch %d/%d with cost = %.5f\n',...
+            fprintf('Running iteration %d/%d with cost = %.5f\n',...
                     iter, opt.MaxIter, cost);
         end
         prev_cost = cost;
+        alpha = alpha*opt.alpha_decay;
     end 
     fprintf('Converged in %d iterations with cost = %.5f!\n', iter, cost);
   
